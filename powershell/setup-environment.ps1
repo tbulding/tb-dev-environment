33278f593536753f27c3cb6d94a720a19ff299a2
Tony Bulding <tbulding@amazon.com>
2019-12-10 15:24:17 -0600

    sdfdsfg

--- powershell/setup-environment.ps1
+++ powershell/setup-environment.ps1
@@ -19,17 +19,20 @@ Add-AppxPackage C:\temp\Ubuntu.appx
 & ubuntu1804 config --default-user tbulding
 $wslpath = (Get-ChildItem HKCU:\Software\Microsoft\Windows\CurrentVersion\Lxss | ForEach-Object { Get-ItemProperty $_.PSPath }) | Select-Object DistributionName, BasePath
 Copy-Item "$sourcepath\wsl\wsl.conf" -Destination "$($wslpath.BasePath)\rootfs\etc"
+New-Item -Path "$($wslpath.BasePath)\rootfs\" -Name "c" -ItemType "directory"
 #endregion
 
 #region Windows Config
 #Font
-Invoke-WebRequest -Uri https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20Nerd%20Font%20Complete.ttf -OutFile 'c:\temp\DejaVu Sans Mono Nerd Font Complete.ttf' -UseBasicParsing
-$sa = new-object -comobject shell.application
 $fontName = 'DejaVu Sans Mono Nerd Font Complete.ttf'
+$fontWebName = $fontName.Replace(" ", "%20")
+$fontWebName
 If ((Test-Path "c:\windows\fonts\$($fontName)") -eq $False) {
     Write-Output "The font $fontName is already installed - Skipping"
 }
 else {
+    Invoke-WebRequest -Uri "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DejaVuSansMono/Regular/complete/$($fontName.Replace(" ", "%20"))" -OutFile "c:\temp\$fontName" -UseBasicParsing
+    $sa = new-object -comobject shell.application
     $Fonts = $sa.NameSpace(0x14)
     $Fonts.CopyHere("c:\temp\$fontName")
 }
